183978b2027db82ea6124bc4d5e28d19a0744c50
Sophia-Cy <sophiaodiase270@gmail.com>
2023-04-25 10:27:43 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+  *print_to_98 - prints all natural numbers from n to 98
+  *followed by a new line
+  *@n: print from this number
+  */
+void print_to_98(int n)
+{
+	int i, j;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+			{
+				_putschar(48 +n);
+			}
+			else if (n == 98)
+			{
+				_putschar(48 + n '\n');
+			}
+		}
+		else if (n >= 98)
+		{
+			for (j = n; j >= 98; j--);
+			{
+				if (j != 98)
+				{
+					_putschar(48 + j);
+				}
+				else if (j == 98)
+				{
+					_putchar(48 + j);
+				}
+                          }
+	       }
+	}
+}
