8fd891538b7d2281a32bc6be78b380b2862aa7eb
Sophia-Cy <sophiaodiase270@gmail.com>
2023-04-28 08:37:33 -0700

    Task 9 fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,33 +12,17 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
-		{
-			printf("Fizz");
-			printf(" ");
-		}
-		else if (i % 5 == 0 && i % 3 != 0)
-		{
-			printf("Buzz");
-			printf(" ");
-		}
-		else if (i % 3 == 0 && i % 5 == 0)
-		{
+		if (i % 15 == 0)
 			printf("FizzBuzz");
-			printf(" ");
-		}
-		else if (i == 1)
-		{
-			printf("%d", i);
-			printf(" ");
-		}
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
 		else
-		{
-			printf("%d", i);
+			printf("%i", i);
+		if (i < 100)
 			printf(" ");
-		}
 	}
-		printf("\n");
-
-		return (0);
+	printf("\n");
+	return (0);
 }
